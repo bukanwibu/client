128930de56f293b6243ab75cf7b9413285abf781
Nico Audy <nicoaudy1994@gmail.com>
2020-04-09 09:34:15 +0700

    initial commit & a bit play with element ui

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">Awesome Project</el-menu-item>
+      <el-menu-item index="2">Pricing</el-menu-item>
+      <el-menu-item index="3">Login</el-menu-item>
+    </el-menu>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+  html {
+    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+      "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>
+      <el-row>
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger">Danger</el-button>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style></style>
