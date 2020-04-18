68e38cfb4c8890aac592bb4d2812360d3105f921
Nico Audy <nicoaudy1994@gmail.com>
2020-04-18 16:11:34 +0700

    margin border on histories

--- pages/histories.vue
+++ pages/histories.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container">
     <div class="my-3 p-3 bg-white rounded shadow-sm">
-      <h6 class="border-bottom border-gray pb-2 mb-0">
+      <h4 class="pb-2 mb-0 text-center">
         Histories ({{ histories.length }})
-      </h6>
+      </h4>
+      <div class="border-bottom border-gray mt-5 mb-2"></div>
       <div class="media text-muted pt-3" v-for="(history, i) in histories">
         <svg
           class="bd-placeholder-img mr-2 rounded"
@@ -19,7 +20,7 @@
           <rect width="100%" height="100%" fill="#007bff" />
           <text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text>
         </svg>
-        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <p class="media-body pb-3 mb-0 small lh-125">
           <strong class="d-block text-gray-dark"
             >{{ history.createdAt | moment }}</strong
           >
